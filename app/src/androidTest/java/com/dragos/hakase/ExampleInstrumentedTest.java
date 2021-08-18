0f34095c1a639ce880d69d21350815f2ff038477
Dragos Dogaru <dragosdog@gmail.com>
2021-08-18 19:43:18 +0900

    Android basic activity transition skeleton

--- app/src/androidTest/java/com/dragos/hakase/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dragos/hakase/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.hakase;
+package com.dragos.hakase;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.hakase", appContext.getPackageName());
+        assertEquals("com.dragos.hakase", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/dragos/hakase/ExampleUnitTest.java
+++ app/src/test/java/com/dragos/hakase/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.hakase;
+package com.dragos.hakase;
 
 import org.junit.Test;
 
